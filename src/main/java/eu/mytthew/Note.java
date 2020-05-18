aeae23ca027ce7d0e9aa9466ba35571864550e09
Mytthew <dm.mateusz@gmail.com>
2020-05-19 01:41:24 +0200

    Version 1.0 works fine

--- src/main/java/eu/mytthew/Note.java
+++ src/main/java/eu/mytthew/Note.java
@@ -1,11 +1,15 @@
 package eu.mytthew;
 
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Note {
 	private static int globalID = 0;
 	private final String title;
 	private final String content;
 	private final int ID = globalID++;
+	private LocalDateTime localDateTime = LocalDateTime.now();
 
 	public Note(String title, String content) {
 		this.title = title;
@@ -19,6 +23,7 @@ public class Note {
 	@Override
 	public String toString() {
 		return "ID: " + ID +
+				"\nDate: " + localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) +
 				"\nTitle: " + title +
 				"\nContent: '" + content + '\'' + "\n";
 	}
