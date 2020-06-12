f58112c306689f9977d3f1473163d32adada47a7
Mytthew <dm.mateusz@gmail.com>
2020-06-12 22:36:10 +0200

    [skip ci] WIP: Map instead of switch

--- src/main/java/eu/mytthew/notepad/Main.java
+++ src/main/java/eu/mytthew/notepad/Main.java
@@ -5,7 +5,6 @@ import eu.mytthew.notepad.auth.RuntimeAuthService;
 import eu.mytthew.notepad.entity.Note;
 import eu.mytthew.notepad.entity.User;
 
-import java.lang.reflect.Method;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
@@ -69,119 +68,111 @@ public class Main {
 	}
 
 	public static void display(IAuthService authService) {
-		boolean repeat = true;
-		while (repeat) {
-			System.out.println("Menu:");
-			System.out.println("1. Add note");
-			System.out.println("2. Review notes");
-			System.out.println("3. Remove note");
-			System.out.println("4. Edit note");
-			System.out.println("5. Change login");
-			System.out.println("6. Change password");
-			System.out.println("7. Logout");
-			System.out.println("0. Exit");
-			String selection = scanner.nextLine();
-			Map<Method, Method> choice = new HashMap<>();
-
-
-			switch (selection) {
-				case "1":
-					System.out.println("Note title: ");
-					String title = scanner.nextLine();
-					System.out.println("Your note:");
-					String content = scanner.nextLine();
-					System.out.println("Set date [yyyy-MM-dd]:");
-					String date = scanner.nextLine();
-					User loggedUser = authService.getLoggedUser();
-					if (date.equals("")) {
-						date = String.valueOf(LocalDate.now());
-						loggedUser.addNote(new Note(title, content, LocalDate.parse(date)));
-						System.out.println("Note added successfully!");
-					} else if (date.matches("^\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])$")) {
-						loggedUser.addNote(new Note(title, content, LocalDate.parse(date)));
-						System.out.println("Note added successfully!");
-					} else {
-						System.out.println("Wrong date format.");
-					}
-					break;
-				case "2":
-					if (authService.getLoggedUser().getNotes().isEmpty()) {
-						System.out.println("No notes.");
-					} else {
-						displayNotes(authService.getLoggedUser());
-					}
-					break;
-				case "3":
-					if (authService.getLoggedUser().getNotes().isEmpty()) {
-						System.out.println("No notes.");
-					} else {
-						System.out.println("Select note: ");
-						String id = scanner.nextLine();
-						User user = authService.getLoggedUser();
-						if (user.removeNote(user.getNotes().get(Integer.parseInt(id)).getUuid())) {
-							System.out.println("Note removed.");
-						} else {
-							System.out.println("Note with this id doesn't exist.");
-						}
-					}
-					break;
-				case "4":
-					if (authService.getLoggedUser().getNotes().isEmpty()) {
-						System.out.println("No notes.");
-					} else {
-						System.out.println("Select note to edit:");
-						String selectedNote = scanner.nextLine();
-						User user = authService.getLoggedUser();
-						if (selectedNote.equals("")) {
-							break;
-						} else if (Integer.parseInt(selectedNote) < user.getNotes().size()) {
-							System.out.println("New title:");
-							String newTitle = scanner.nextLine();
-							System.out.println("New content:");
-							String newContent = scanner.nextLine();
-							System.out.println("New date:");
-							String newDate = scanner.nextLine();
-							editNote(user.getNotes().get(Integer.parseInt(selectedNote)), newTitle, newContent, newDate);
-							System.out.println("Note changed successfully.");
-						} else {
-							System.out.println("Note with this id doesn't exist.");
-						}
-					}
-					break;
-				case "5":
-					System.out.println("Type new login: ");
-					String newNickname = scanner.nextLine();
-					if (authService.containsNickname(newNickname)) {
-						System.out.println("This nickname is already taken.");
-					} else {
-						String oldNickname = authService.getLoggedUser().getNickname();
-						authService.getLoggedUser().setNickname(newNickname);
-						System.out.println("Nickname changed successfully!");
-						System.out.println("Old nickname: " + oldNickname);
-						System.out.println("New nickname: " + newNickname);
-					}
-					break;
-				case "6":
-					System.out.println("Type current password:");
-					String currentPassword = scanner.nextLine();
-					System.out.println("Type new password:");
-					String newPassword = scanner.nextLine();
-					if (authService.changePassword(currentPassword, newPassword)) {
-						System.out.println("Password changed successfully.");
-					} else {
-						System.out.println("Wrong current password.");
-					}
-					break;
-				case "7":
-					System.out.println("Logged out.");
-					return;
-				case "0":
-					System.exit(0);
-				default:
-					System.out.println("Wrong choice.");
-					break;
+		System.out.println("Menu:");
+		System.out.println("1. Add note");
+		System.out.println("2. Review notes");
+		System.out.println("3. Remove note");
+		System.out.println("4. Edit note");
+		System.out.println("5. Change login");
+		System.out.println("6. Change password");
+		System.out.println("7. Logout");
+		System.out.println("0. Exit");
+		String selection = scanner.nextLine();
+		Map<String, Runnable> menu = new HashMap<>();
+		menu.put("1", () -> {
+			System.out.println("Note title: ");
+			String title = scanner.nextLine();
+			System.out.println("Your note:");
+			String content = scanner.nextLine();
+			System.out.println("Set date [yyyy-MM-dd]:");
+			String date = scanner.nextLine();
+			User loggedUser = authService.getLoggedUser();
+			if (date.equals("")) {
+				date = String.valueOf(LocalDate.now());
+				loggedUser.addNote(new Note(title, content, LocalDate.parse(date)));
+				System.out.println("Note added successfully!");
+			} else if (date.matches("^\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])$")) {
+				loggedUser.addNote(new Note(title, content, LocalDate.parse(date)));
+				System.out.println("Note added successfully!");
+			} else {
+				System.out.println("Wrong date format.");
 			}
-		}
+		});
+		menu.put("2", () -> {
+			if (authService.getLoggedUser().getNotes().isEmpty()) {
+				System.out.println("No notes.");
+			} else {
+				displayNotes(authService.getLoggedUser());
+			}
+		});
+		menu.put("3", () -> {
+			if (authService.getLoggedUser().getNotes().isEmpty()) {
+				System.out.println("No notes.");
+			} else {
+				System.out.println("Select note: ");
+				String id = scanner.nextLine();
+				User user = authService.getLoggedUser();
+				if (user.removeNote(user.getNotes().get(Integer.parseInt(id)).getUuid())) {
+					System.out.println("Note removed.");
+				} else {
+					System.out.println("Note with this id doesn't exist.");
+				}
+			}
+		});
+		menu.put("4", () -> {
+			if (authService.getLoggedUser().getNotes().isEmpty()) {
+				System.out.println("No notes.");
+			} else {
+				System.out.println("Select note to edit:");
+				String selectedNote = scanner.nextLine();
+				User user = authService.getLoggedUser();
+				if (selectedNote.equals("")) {
+
+				} else if (Integer.parseInt(selectedNote) < user.getNotes().size()) {
+					System.out.println("New title:");
+					String newTitle = scanner.nextLine();
+					System.out.println("New content:");
+					String newContent = scanner.nextLine();
+					System.out.println("New date:");
+					String newDate = scanner.nextLine();
+					editNote(user.getNotes().get(Integer.parseInt(selectedNote)), newTitle, newContent, newDate);
+					System.out.println("Note changed successfully.");
+				} else {
+					System.out.println("Note with this id doesn't exist.");
+				}
+			}
+		});
+		menu.put("5", () -> {
+			System.out.println("Type new login: ");
+			String newNickname = scanner.nextLine();
+			if (authService.containsNickname(newNickname)) {
+				System.out.println("This nickname is already taken.");
+			} else {
+				String oldNickname = authService.getLoggedUser().getNickname();
+				authService.getLoggedUser().setNickname(newNickname);
+				System.out.println("Nickname changed successfully!");
+				System.out.println("Old nickname: " + oldNickname);
+				System.out.println("New nickname: " + newNickname);
+			}
+		});
+		menu.put("6", () -> {
+			System.out.println("Type current password:");
+			String currentPassword = scanner.nextLine();
+			System.out.println("Type new password:");
+			String newPassword = scanner.nextLine();
+			if (authService.changePassword(currentPassword, newPassword)) {
+				System.out.println("Password changed successfully.");
+			} else {
+				System.out.println("Wrong current password.");
+			}
+		});
+		menu.put("7", () -> {
+			System.out.println("Logged out.");
+			return;
+		});
+		menu.put("0", () -> {
+			System.exit(0);
+		});
 	}
 
 	public static void displayNotes(User user) {
