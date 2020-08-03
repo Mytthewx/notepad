23352794a29212ea41a6a69c14bddd19fecb325c
Mytthew <dm.mateusz@gmail.com>
2020-08-04 00:14:28 +0200

    [skip ci] WIP: Trying to fix tests

--- src/test/java/eu/mytthew/notepad/auth/FileAuthServiceTest.java
+++ src/test/java/eu/mytthew/notepad/auth/FileAuthServiceTest.java
@@ -1,12 +1,20 @@
 package eu.mytthew.notepad.auth;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class FileAuthServiceTest {
 	@Test
 	public void addUserFalseTest() {
@@ -23,16 +31,38 @@ public class FileAuthServiceTest {
 	}
 
 	@Test
-	public void addUserTrueTest() {
+	public void addUserTrueWithVerifyTest() {
 		// given
 		FileOperation fileOperation = mock(FileOperation.class);
 		IAuthService authService = new FileAuthService(fileOperation);
-		when(fileOperation.createFile(any(), any())).thenReturn(false);
+		when(fileOperation.createFile(any(), any())).thenReturn(true);
 
 		// when
 		boolean result = authService.addUser("UserTest", "123");
 
 		// then
-		assertFalse(result);
+		assertTrue(result);
+		verify(fileOperation, times(1)).createFile(any(), any());
+	}
+
+	@Test
+	public void addUserTrueTest() {
+		// given
+		FileOperation fileOperation = mock(FileOperation.class);
+		IAuthService authService = new FileAuthService(fileOperation);
+		JSONObject jsonObject = new JSONObject();
+		JSONObject jsonObject2 = new JSONObject();
+		jsonObject.put("nick", "UserTest");
+		jsonObject.put("pass", "123");
+		jsonObject2.put("nick", "UserTest");
+		jsonObject2.put("pass", "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3");
+		when(fileOperation.createFile(eq("UserTest"), eq(jsonObject))).thenReturn(true);
+
+		// when
+		boolean result = authService.addUser("UserTest", "123");
+
+		// then
+
+		verify(fileOperation, times(1)).createFile(eq("UserTest"), eq(jsonObject2));
 	}
 }
