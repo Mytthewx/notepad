442bad5f6206dd7e67647e72a07dc6f0aa7289a3
mateusz.domagala <mateusz.domagala@eleader.biz>
2020-09-16 13:07:12 +0200

    Login with hashPassword and containsNickname

--- src/main/java/eu/mytthew/notepad/auth/DatabaseAuthService.java
+++ src/main/java/eu/mytthew/notepad/auth/DatabaseAuthService.java
@@ -1,7 +1,10 @@
 package eu.mytthew.notepad.auth;
 
+import com.google.common.hash.Hashing;
 import eu.mytthew.notepad.entity.User;
+import lombok.Getter;
 
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,39 +13,58 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseAuthService implements IAuthService {
-	String url = "jdbc:mysql://localhost:3306/notepad";
-	String user = "root";
-	String dbpassword = "";
+	Connection connection = null;
+	@Getter
+	private User loggedUser;
+	private String oldNickname;
+
+	public Connection connectToDatabase() {
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/notepad", "root", "");
+			return connection;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	@Override
 	public boolean containsNickname(String nickname) {
+		String sql = "SELECT login FROM users WHERE login = '" + nickname + "'";
+		connectToDatabase();
 		try {
-			Connection con = DriverManager.getConnection(url, user, dbpassword);
-			String sql = "SELECT login FROM users WHERE login = '" + nickname + "'";
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			if (rs.next()) {
-				con.close();
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.execute();
+			if (preparedStatement.getResultSet().next()) {
 				return true;
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
 		}
 		return false;
 	}
 
 	@Override
 	public boolean login(String nickname, String password) {
-		try {
-			Connection con = DriverManager.getConnection(url, user, dbpassword);
-			String userPassword = "SELECT password FROM users WHERE login = '" + nickname + "'";
-			String sql = "SELECT * FROM users WHERE login = '" + nickname + "'";
-			PreparedStatement preparedStatement = con.prepareStatement(sql);
-			preparedStatement.execute();
-			con.close();
-			return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
+		connectToDatabase();
+		if (containsNickname(nickname)) {
+			try {
+				String userPasswordSQL = "SELECT password FROM users WHERE login = '" + nickname + "'";
+				PreparedStatement userPassword = connection.prepareStatement(userPasswordSQL);
+				userPassword.execute();
+				if (userPassword.getResultSet().next()) {
+					if (!userPassword.getResultSet().getString("password").equals(hashPassword(password))) {
+						return false;
+					}
+					String sql = "SELECT * FROM users WHERE login = '" + nickname + "'";
+					PreparedStatement preparedStatement = connection.prepareStatement(sql);
+					preparedStatement.execute();
+					loggedUser = new User(nickname, password);
+					return true;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		return false;
 	}
@@ -54,20 +76,34 @@ public class DatabaseAuthService implements IAuthService {
 
 	@Override
 	public boolean changeNickname(String newNickname) {
+		if (!containsNickname(newNickname)) {
+			try {
+				String sql = "UPDATE users SET login = '" + newNickname + "'" + " WHERE login = '" + oldNickname + "'";
+				connectToDatabase();
+				Statement st = connection.createStatement();
+				ResultSet rs = st.executeQuery(sql);
+				if (rs.next()) {
+					return true;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean addUser(String nickname, String userPassword) {
+		if (containsNickname(nickname)) {
+			return false;
+		}
 		try {
-			Connection con = DriverManager.getConnection(url, user, dbpassword);
-			String sql =
-					"INSERT INTO users(login, password) VALUES (?, ?);";
-			PreparedStatement preparedStatement = con.prepareStatement(sql);
+			String sql = "INSERT INTO users(login, password) VALUES (?, ?);";
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
 			preparedStatement.setString(1, nickname);
-			preparedStatement.setString(2, userPassword);
+			preparedStatement.setString(2, hashPassword(userPassword));
 			preparedStatement.execute();
-			con.close();
 			return true;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -77,11 +113,16 @@ public class DatabaseAuthService implements IAuthService {
 
 	@Override
 	public boolean logout() {
-		return false;
+		loggedUser = null;
+		try {
+			connection.close();
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		}
+		return true;
 	}
 
-	@Override
-	public User getLoggedUser() {
-		return null;
+	private String hashPassword(String password) {
+		return Hashing.sha256().hashString(password, StandardCharsets.UTF_8).toString();
 	}
 }
