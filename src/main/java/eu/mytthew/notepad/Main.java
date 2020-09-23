e1d961f42e7b23ab15b4b27ecec8b0697e7b1eca
mateusz.domagala <mateusz.domagala@eleader.biz>
2020-09-23 14:50:19 +0200

    [skip ci] WIP: Trying to add noteservice

--- src/main/java/eu/mytthew/notepad/Main.java
+++ src/main/java/eu/mytthew/notepad/Main.java
@@ -172,7 +172,8 @@ public class Main {
 		User loggedUser = authService.getLoggedUser();
 		if (date.equals("")) {
 			date = String.valueOf(LocalDate.now());
-			loggedUser.addNote(new Note(title, content, LocalDate.parse(date)));
+			Note note = new Note(title, content, LocalDate.parse(date));
+			loggedUser.addNote(note);
 			noteService.addNoteToDatabase(connection, loggedUserId, note);
 			System.out.println("Note added successfully!");
 		} else if (date.matches("^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$")) {
--- src/main/java/eu/mytthew/notepad/auth/NoteService.java
+++ src/main/java/eu/mytthew/notepad/auth/NoteService.java
@@ -2,32 +2,18 @@ package eu.mytthew.notepad.auth;
 
 import eu.mytthew.notepad.entity.Note;
 import eu.mytthew.notepad.entity.User;
-import lombok.Getter;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
-public class DatabaseOperations {
-	@Getter
-	Connection connection = null;
-
-	public Connection connectToDatabase() {
-		try {
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/notepad", "root", "");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return connection;
-	}
-
-	public void readNotes(Connection connection, User user, int loggedUserid) {
+public class NoteService {
+	public void readNotes(Connection connection, User user, int loggedUserId) {
 		try {
 			String notesSQL = "SELECT * FROM notes WHERE user_id = ?";
 			PreparedStatement notesStatement = connection.prepareStatement(notesSQL);
-			notesStatement.setInt(1, loggedUserid);
+			notesStatement.setInt(1, loggedUserId);
 			notesStatement.execute();
 			while (notesStatement.getResultSet().next()) {
 				String title = notesStatement.getResultSet().getString("title");
@@ -35,6 +21,20 @@ public class DatabaseOperations {
 				String localDate = notesStatement.getResultSet().getString("date");
 				user.addNote(new Note(title, content, LocalDate.parse(localDate)));
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void addNoteToDatabase(Connection connection, int loggedUserId, Note note) {
+		String noteToSQL = "INSERT INTO notes(title, content, date, user_id) VALUES (?, ?, ?, ?)";
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(noteToSQL);
+			preparedStatement.setString(1, note.getTitle());
+			preparedStatement.setString(2, note.getContent());
+			preparedStatement.setString(3, "2020-10-10");
+			preparedStatement.setInt(4, loggedUserId);
+			preparedStatement.execute();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
