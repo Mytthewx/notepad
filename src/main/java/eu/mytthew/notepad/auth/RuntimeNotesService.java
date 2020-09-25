569e85db7f9f40e895465a01c82f79947b14a594
mateusz.domagala <mateusz.domagala@eleader.biz>
2020-09-25 13:37:13 +0200

    [skip ci] WIP: RNS

--- src/main/java/eu/mytthew/notepad/auth/RuntimeNotesService.java
+++ src/main/java/eu/mytthew/notepad/auth/RuntimeNotesService.java
@@ -5,10 +5,12 @@ import eu.mytthew.notepad.entity.Note;
 import eu.mytthew.notepad.entity.User;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class RuntimeNotesService implements INotesService {
+	private final List<Note> noteList = new ArrayList<>();
 	private User loggedUser;
 
 	@Override
@@ -18,30 +20,33 @@ public class RuntimeNotesService implements INotesService {
 		String content = note.getContent();
 		LocalDate localDate = note.getNoteDate();
 		Note newNote = new Note(id, title, content, localDate);
-		loggedUser.addNote(newNote);
+		noteList.add(newNote);
 	}
 
 	@Override
 	public void editNote(int noteId, String newTitle, String newContent, String newDate) {
-		if (!newTitle.equals("")) {
-			loggedUser.getNotes().get(noteId).setTitle(newTitle);
-		}
-		if (!newContent.equals("")) {
-			loggedUser.getNotes().get(noteId).setTitle(newContent);
-		}
-		if (!newDate.equals("")) {
-			loggedUser.getNotes().get(noteId).setNoteDate(LocalDate.parse(newDate));
+		Optional<Note> optionalNote = noteList.stream().filter(note -> note.getId() == noteId).findAny();
+		if (optionalNote.isPresent()) {
+			if (!newTitle.equals("")) {
+				optionalNote.get().setTitle(newTitle);
+			}
+			if (!newContent.equals("")) {
+				optionalNote.get().setTitle(newContent);
+			}
+			if (!newDate.equals("")) {
+				optionalNote.get().setNoteDate(LocalDate.parse(newDate));
+			}
 		}
 	}
 
 	@Override
 	public boolean removeNote(int id) {
-		Optional<Note> optionalNote = loggedUser.getNotes()
+		Optional<Note> optionalNote = noteList
 				.stream()
 				.filter(note -> note.getId() == id)
 				.findAny();
 		if (optionalNote.isPresent()) {
-			loggedUser.removeNote(optionalNote.get());
+			noteList.remove(optionalNote.get());
 			return true;
 		}
 		return false;
@@ -54,8 +59,7 @@ public class RuntimeNotesService implements INotesService {
 
 	@Override
 	public boolean noteWithThisIdExist(int id) {
-		return loggedUser
-				.getNotes()
+		return noteList
 				.stream()
 				.anyMatch(note -> note.getId() == id);
 	}
@@ -73,12 +77,12 @@ public class RuntimeNotesService implements INotesService {
 	@Override
 	public List<Note> getAllNotes(User user) {
 		loggedUser = user;
-		return user.getNotes();
+		return noteList;
 	}
 
 	@Override
 	public List<Reminder> getAllReminders(int noteId) {
-		return loggedUser.getNotes().get(noteId).getReminders();
+		return noteList.get(noteId).getReminders();
 	}
 
 	@Override
@@ -98,7 +102,7 @@ public class RuntimeNotesService implements INotesService {
 
 	@Override
 	public boolean checkIfUserHasNoteWithId(int idUser, int idNote) {
-		return loggedUser.getNotes()
+		return noteList
 				.stream()
 				.anyMatch(note -> note.getId() == idNote);
 	}
--- src/main/java/eu/mytthew/notepad/entity/User.java
+++ src/main/java/eu/mytthew/notepad/entity/User.java
@@ -3,13 +3,8 @@ package eu.mytthew.notepad.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 
 public class User {
-	private final List<Note> notes = new ArrayList<>();
 	@Getter
 	private int id;
 	@Getter
@@ -27,21 +22,5 @@ public class User {
 	public User(String nickname, String password) {
 		this.nickname = nickname;
 		this.password = password;
-	}
-
-	public void addNote(Note note) {
-		notes.add(note);
-	}
-
-	public List<Note> getNotes() {
-		return Collections.unmodifiableList(notes);
-	}
-
-	public boolean removeNote(Note note) {
-		if (notes.isEmpty()) {
-			return false;
-		}
-		notes.remove(note);
-		return true;
 	}
 }
